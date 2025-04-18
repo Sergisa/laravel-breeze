cbeb855c0b69b1a6dbfbadc9dbf62a62eab7adc4
Sergisa <phebra@yandex.ru>
2025-04-18 13:57:25 +0300

    Добавлен пакет иконок

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
+                    <x-heroicon-o-plus x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')" class="w-6 h-6 hover:bg-sky-100 rounded-md"/>
                     @auth
                         <a href="{{ url('/dashboard') }}" class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
                             Dashboard
